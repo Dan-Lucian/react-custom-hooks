3bc61a5dd4e692ea49b1d74981a81de93f130c0a
Dan-Lucian <danugaina1403@gmail.com>
2022-01-12 12:12:11 +0200

    refactor: useOutsideClick's example box changes colors

--- src/useOutsideClick/ExampleUseOutsideClick.js
+++ src/useOutsideClick/ExampleUseOutsideClick.js
@@ -5,14 +5,17 @@ const ExampleUseOutsideClick = () => {
   // returns a ref to be put on the component
   const [refContainer, isClickOutside] = useOutsideClick();
 
+  const stylesBox = {
+    width: '100px',
+    height: '100px',
+    backgroundColor: isClickOutside ? 'blue' : 'red',
+  };
+
   return (
     <Example title="useOutsideClick">
       <p>The text below tells where you clicked relative to the black box</p>
       <p>clicked {isClickOutside ? 'outside' : 'inside'}</p>
-      <div
-        ref={refContainer}
-        style={{ width: '100px', height: '100px', backgroundColor: '#000' }}
-      />
+      <div ref={refContainer} style={stylesBox} />
     </Example>
   );
 };
